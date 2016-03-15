15d35aa62fdb8676661fe5447490f141ee6dffe0
Mr.Yao <348892914@qq.com>
2016-03-15 19:35:49 +0800

    init some object

--- DefenseOfTheAncients/Controllers/Tabbar/DTTabBarViewController.m
+++ DefenseOfTheAncients/Controllers/Tabbar/DTTabBarViewController.m
@@ -10,9 +10,9 @@
 
 #import "DTInformationViewController.h"
 #import "DTTreasureBoxViewController.h"
-#import "DTMineViewController.h"
 #import "DTGameViewController.h"
 #import "DTVideoViewController.h"
+#import "DTMineViewController.h"
 
 @interface DTTabBarViewController ()
 
@@ -22,8 +22,44 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // 定制外观
+    self.tabBar.tintColor = [UIColor colorWithRed:255/255.0f green:14/255.0f blue:149/255.0f alpha:1.0f];
+    self.tabBar.translucent = NO;
+    
+    DTInformationViewController *informationVC = [[DTInformationViewController alloc] init];
+    informationVC.tabBarItem.image = [UIImage imageNamed:@"tabbar_3_normal"];
+    informationVC.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_3_pressed"];
+    informationVC.tabBarItem.title = @"资讯";
+    UINavigationController *informationNavC = [[UINavigationController alloc] initWithRootViewController:informationVC];
+    
+    DTTreasureBoxViewController *treasureBoxVC = [[DTTreasureBoxViewController alloc] init];
+    treasureBoxVC.tabBarItem.image = [UIImage imageNamed:@"tabbar_5_normal"];
+    treasureBoxVC.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_5_pressed"];
+    treasureBoxVC.tabBarItem.title = @"宝盒";
+    UINavigationController *treasureBoxNavC = [[UINavigationController alloc] initWithRootViewController:treasureBoxVC];
+    
+    DTGameViewController *gameVC = [[DTGameViewController alloc] init];
+    gameVC.tabBarItem.image = [UIImage imageNamed:@"tabbar_1_normal"];
+    gameVC.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_1_pressed"];
+    gameVC.tabBarItem.title = @"赛事";
+    UINavigationController *gameNavC = [[UINavigationController alloc] initWithRootViewController:gameVC];
+    
+    DTVideoViewController *videoVC = [[DTVideoViewController alloc] init];
+    videoVC.tabBarItem.image = [UIImage imageNamed:@"tabbar_4_normal"];
+    videoVC.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_4_pressed"];
+    videoVC.tabBarItem.title = @"视频";
+    UINavigationController *videoNavC = [[UINavigationController alloc] initWithRootViewController:videoVC];
+    
+    DTMineViewController *mineVC = [[DTMineViewController alloc] init];
+    mineVC.tabBarItem.image = [UIImage imageNamed:@"tabbar_2_normal"];
+    mineVC.tabBarItem.selectedImage = [UIImage imageNamed:@"tabbar_2_pressed"];
+    mineVC.tabBarItem.title = @"我的";
+    UINavigationController *mineNavC = [[UINavigationController alloc] initWithRootViewController:mineVC];
+    
+    self.viewControllers = @[informationNavC,treasureBoxNavC,videoNavC,gameNavC,mineNavC];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+  platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+# use_frameworks!
+
+target 'DefenseOfTheAncients' do
+   pod 'AFNetworking', '~> 3.0.4'
+   pod 'MMProgressHUD', '~> 0.3.2'
+   pod 'MJRefresh', '~> 3.1.0'
+   pod 'MJExtension', '~> 3.0.10'
+end
+
+target 'DefenseOfTheAncientsTests' do
+
+end
+
+target 'DefenseOfTheAncientsUITests' do
+
+end
+
